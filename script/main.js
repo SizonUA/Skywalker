b69dd44518968eb39b93980b0ee2138157aadf77
Sizon <sizon@vidrada.org>
2020-04-22 16:15:03 +0300

    fixed slider bug

--- script/main.js
+++ script/main.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
-  // Handler for .ready() called.
   $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 5,
-    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    slideTransition: `linear`,
+    navClass: ['slider__nav--left', 'slider__nav--right'],
     responsive: {
       0: {
         items: 1
       },
-      600: {
+      700: {
+        items: 2
+      },
+      900: {
         items: 3
       },
-      1000: {
+      1200: {
         items: 4
       }
     }
